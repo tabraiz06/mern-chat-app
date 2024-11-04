29036be96dd8ee5b504359b2d2d881cafb6b1dae
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-11-04 21:35:14 +0530

    register comp completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import "./App.css";
 import Left from "./components/left/Left1/Left";
 import Right from "./components/right/Right";
 import Left2 from "./components/left/left2/Left2";
+import Register from "./components/credentials/Register";
 
 function App() {
   return (
     <div className="h-screen flex w-full overflow-hidden scroll-none">
-      <Left2 />
+      {/* <Left2 />
       <Left />
-      <Right />
+      <Right /> */}
+      <Register/>
     </div>
   );
 }
